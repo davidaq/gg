bfd4e1de7d559a2257bcc5d4445a32cb5333ef5e
DavidAQ <aq@num16.com>
2014-11-02 19:40:49 +0800

    ho

--- res/index.js
+++ res/index.js
@@ -5,14 +5,14 @@ $(function() {
     if(match) {
         document.location.hash = match[1];
     }
-    fetchSearchResult();
-    setInterval(fetchSearchResult, 500);
+    checkSearch();
+    setInterval(checkSearch, 500);
 });
 function search(form) {
     var keyword = $('.searchbox', form).val();
     document.location.hash = encodeURIComponent(keyword);
 }
-var fetchSearchResult = function() {
+var checkSearch = function() {
     var currentHash = '';
     return function() {
         var keyword = document.location.hash;
@@ -21,14 +21,54 @@ var fetchSearchResult = function() {
         currentHash = keyword;
         if(keyword[0] == '#')
             keyword = keyword.substr(1);
+        fetchSearchResult(keyword, 1);
+    };
+}();
+var fetchSearchResult = function() {
+    var prevKeyword = '';
+    var loading = false;
+    var step = 0;
+    setInterval(function() {
+        if(loading) {
+            step = (step + 1) & 31;
+            $('h1.wave').css({opacity:Math.abs(step / 16 - 1)});
+        } else {
+            step = 0;
+            $('h1.wave').css({opacity:1});
+        }
+    }, 30);
+    return function(keyword, page) {
+        if(false === keyword)
+            keyword = prevKeyword;
+        else {
+            prevKeyword = keyword;
+            $('#page_card_result .pages').hide();
+        }
+        if(!page) page = 1;
         keyword = keyword.trim();
+        $(document).scrollTop(0);
         if(keyword.length > 0) {
             $('#page_card_home').hide();
             $('#page_card_result').show();
             $('#page_card_result .searchbox').val(decodeURIComponent(keyword));
+            loading = true;
+            $.get('search.php', {keyword:keyword,page:page}, function(result) {
+                loading = false;
+                $('#searchresult').html(result);
+                $('#page_card_result .pages').show();
+                $('#page_card_result .pages a').removeClass('active');
+                page--;
+                $('#page_card_result .pages a:eq(' + page + ')').addClass('active');
+                setTimeout(function() {
+                    $('.related a').each(function() {
+                        var u = encodeURIComponent($(this).text());
+                        $(this).attr('href', '#' + u);
+                    });
+                }, 1);
+            }, 'html');
         } else {
             $('#page_card_home').show();
             $('#page_card_result').hide();
         }
-    };
+    }
 }();
--- search.php
+++ search.php
@@ -1,6 +1,33 @@
 <?php
-$url = 'https://www.google.com.au/search?q=' . $_GET['keyword'];
-$UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/534.30 (KHTML, like Gecko) Chrome/12.0.742.112 Safari/534.30';
+header('Content-type:text/html; charset=utf-8');
+$url = 'https://www.google.com.au/search?nfpr=1&spell=&q=' . $_GET['keyword'];
+$start = isset($_GET['page']) ? $_GET['page'] * 10 - 10: 0;
+if($start)
+    $url .= '&start=' . $start;
+$UA = 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_0 like Mac OS X; en-us) AppleWebKit/532.9 (KHTML, like Gecko) Version/4.0.5 Mobile/8A293 Safari/6531.22.7';
 exec("curl --proxy dl.niven.cn:21 -A '$UA' '$url'", $output);
 $output = implode('', $output);
-echo $output;
+$output = preg_replace('/\<script.*>\>.*?<\/script\>/is' ,'', $output);
+$output = preg_replace('/\<style.*?\>.*?<\/style\>/is' ,'', $output);
+$typo = strstr($output, '>Did you mean');
+if($typo) {
+    $output = $typo;
+    echo '<div class="typo">';
+    preg_match('/\<a.*?\>(.*?)\<\/a\>/is', $output, $m);
+    $t = preg_replace('/\<.*?\>/is', '', $m[1]);
+    echo "<a href='#" . urlencode($t) . "'>$t</a>";
+    echo '</div>';
+}
+preg_match_all('/\<li class="g"\>(.*?)\<\/li\>(?:\<li class="g"\>|\<\/ol\>\<\/div\>\<\/div\>\<\/div\>)/is', $output, $match);
+echo '<div class="result">';
+foreach($match[1] as $v) {
+    echo '<div class="item">';
+    echo $v;
+    echo '</div>';
+}
+echo '</div>';
+$output = strstr($output, 'Searches related to');
+preg_match('/\<table.*?\>(.*?)\<\/table\>/is', $output, $m);
+echo '<div class="related">';
+echo $m[1];
+echo '</div>';
