0aec2752b867e78eadb5f371c2f5c03ca59375e6
root <root@AY1401151421114176b8Z.(none)>
2014-11-03 10:18:13 +0800

    search regex bug

--- search.php
+++ search.php
@@ -7,7 +7,8 @@ if($start)
 $UA = 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_0 like Mac OS X; en-us) AppleWebKit/532.9 (KHTML, like Gecko) Version/4.0.5 Mobile/8A293 Safari/6531.22.7';
 exec("curl --proxy dl.niven.cn:21 -A '$UA' '$url'", $output);
 $output = implode('', $output);
-$output = preg_replace('/\<script.*>\>.*?<\/script\>/is' ,'', $output);
+$output = preg_replace('/\<head.*?\>.*?<\/head\>/is' ,'', $output);
+$output = preg_replace('/\<script.*?\>.*?<\/script\>/is' ,'', $output);
 $output = preg_replace('/\<style.*?\>.*?<\/style\>/is' ,'', $output);
 $typo = strstr($output, '>Did you mean');
 if($typo) {
@@ -18,7 +19,7 @@ if($typo) {
     echo "<a href='#" . urlencode($t) . "'>$t</a>";
     echo '</div>';
 }
-preg_match_all('/\<li class="g"\>(.*?)\<\/li\>(?:\<li class="g"\>|\<\/ol\>\<\/div\>\<\/div\>\<\/div\>)/is', $output, $match);
+preg_match_all('/\<li class="g"\>(.*?)\<\/li\>(?=\<li class="g"\>|\<\/ol\>\<\/div\>\<\/div\>\<\/div\>)/is', $output, $match);
 echo '<div class="result">';
 foreach($match[1] as $v) {
     echo '<div class="item">';
